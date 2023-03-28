836c7cb55b9af26b755072e7e2af547314316fc3
YugBhanushali <yughbhanushali241103@gamil.com>
2023-03-29 00:22:41 +0530

    Font fix for mobile view

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
       
     },[canvasBackGround,mainLogo1,mainLogo2,mainLogo3])
   return (
-    <div className='absolute bottom-3'>
+    <div className='fixed bottom-3'>
       <div style={{fontFamily:'JetBrains Mono'}} className='text-[#BBADAD] flex justify-center items-center'>
         <div className='cursor-pointer'>
             Made by <NavLink to='https://github.com/YugBhanushali'><span className={`hover:text-[white]`}>Yug Bhanushali</span></NavLink> 
