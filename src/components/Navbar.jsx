3af438d95b03ae56d5f8db637862d3c07dc85ee6
YugBhanushali <yughbhanushali241103@gamil.com>
2023-03-30 18:50:46 +0530

    added product hunt button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
             }}
         />
       </div>
-      <div className='fixed top-4 right-5 cursor-pointer'>
+      <div className='fixed flex flex-row justify-center items-center top-4 right-[1px] mr-3 lg:mr-0 cursor-pointer'>
         <NavLink to='https://github.com/YugBhanushali/code-to-image'>
 
           <AiFillGithub className='cursor-pointer'onMouseOver={()=>setgitLogo(true)} onMouseOut={()=>setgitLogo(false)}
@@ -38,6 +38,12 @@ const Navbar = () => {
               }}
           />
         </NavLink>
+        <div className='hidden lg:flex'>
+          <a href="https://www.producthunt.com/posts/code-to-image?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-code&#0045;to&#0045;image" target="_blank">
+            <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=386643&theme=light" alt="Code&#0032;to&#0032;Image - Beautify&#0032;your&#0032;code&#0044;&#0032;as&#0032;only&#0032;code&#0032;seems&#0032;to&#0032;be&#0032;boring | Product Hunt"  className='w-[250px] h-[45px]' width="250" height="54" />
+          </a>
+        </div>
+        {/* <a href="https://www.producthunt.com/posts/code-to-image?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-code&#0045;to&#0045;image" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=386643&theme=dark" alt="Code&#0032;to&#0032;Image - Beautify&#0032;your&#0032;code&#0044;&#0032;as&#0032;only&#0032;code&#0032;seems&#0032;to&#0032;be&#0032;boring | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a> */}
       </div>
     </div>
   )
