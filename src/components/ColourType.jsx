beff35a2a60a0a5e6a44b33ea2aa183359ca66d6
YugBhanushali <yughbhanushali241103@gmail.com>
2024-03-02 22:09:04 +0530

    changed bg colour

--- src/components/ColourType.jsx
+++ src/components/ColourType.jsx
@@ -1,22 +1,48 @@
-import React, { useContext, useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { ColourTypeContext } from '../Context/ColourType'
-import { GlobelThemeContext } from '../Context/GlobelThemeContext';
-import { changecanvasColourType } from '../redux/CanvasConfigSlice';
+import React, { useContext, useState } from "react";
+import { useDispatch } from "react-redux";
+import { ColourTypeContext } from "../Context/ColourType";
+import { GlobelThemeContext } from "../Context/GlobelThemeContext";
+import { changecanvasColourType } from "../redux/CanvasConfigSlice";
+import { useEffect } from "react";
 
 const ColourType = () => {
+  const { colourType, setcolourType } = useContext(ColourTypeContext);
+  const { bgTheme, setbgTheme } = useContext(GlobelThemeContext);
+  const dispatch = useDispatch();
 
-  const {colourType,setcolourType} = useContext(ColourTypeContext);
-  const {bgTheme,setbgTheme} = useContext(GlobelThemeContext);
-  const dispatch=useDispatch();
+  useEffect(() => {
+    setcolourType("gradient");
+    setbgTheme("linear");
+    dispatch(changecanvasColourType("linear"));
+  }, []);
   return (
     <>
-      <div className={`flex flex-row justify-center items-center bg-[#1111116b] border-[1px] border-solid border-[#af9f9f9e] ml-4 h-[50px] w-[500px] rounded-[6px]`}>
-            <div onClick={()=>{setcolourType('solid');setbgTheme('solid');dispatch(changecanvasColourType('solid'))}} className={`flex flex-row justify-center items-center  bg-[#1111116b] ${colourType==='solid' ? 'bg-[#5b5a5a6b]' : null} border-[1px] border-solid border-[#af9f9f9e] h-[35px] w-[200px] m-1 rounded-[4px] cursor-pointer`}>Solid</div>
-            <div onClick={()=>{setcolourType('gradient');setbgTheme('linear');dispatch(changecanvasColourType('linear'))}} className={`flex flex-row justify-center items-center bg-[#1111116b] ${colourType==='gradient' ? 'bg-[#5b5a5a6b]' : null} border-[1px] border-solid border-[#af9f9f9e] h-[35px] w-[200px] m-1 rounded-[4px] cursor-pointer`}>Gradient</div>
+      <div
+        className={`ml-4 flex h-[50px] w-[500px] flex-row items-center justify-center rounded-[6px] border-[1px] border-solid border-[#af9f9f9e] bg-[#1111116b]`}
+      >
+        <div
+          onClick={() => {
+            setcolourType("solid");
+            setbgTheme("solid");
+            dispatch(changecanvasColourType("solid"));
+          }}
+          className={`flex flex-row items-center justify-center  bg-[#1111116b] ${colourType === "solid" ? "bg-[#5b5a5a6b]" : null} m-1 h-[35px] w-[200px] cursor-pointer rounded-[4px] border-[1px] border-solid border-[#af9f9f9e]`}
+        >
+          Solid
+        </div>
+        <div
+          onClick={() => {
+            setcolourType("gradient");
+            setbgTheme("linear");
+            dispatch(changecanvasColourType("linear"));
+          }}
+          className={`flex flex-row items-center justify-center bg-[#1111116b] ${colourType === "gradient" ? "bg-[#5b5a5a6b]" : null} m-1 h-[35px] w-[200px] cursor-pointer rounded-[4px] border-[1px] border-solid border-[#af9f9f9e]`}
+        >
+          Gradient
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default ColourType;
--- src/redux/CanvasConfigSlice.js
+++ src/redux/CanvasConfigSlice.js
@@ -1,71 +1,86 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const CanvasConfigSlice = createSlice({
-    name:'canvasStyle',
-    initialState:{
-        canvasPadding:'30px',
-        canvasBackGround:'#6B5ED9',
-        canvasWidth:'800px',
-        snippetFontFamily:`'JetBrains Mono', monospace`,
-        snippetMode:'dark',
-        snippetTheme:'prism-atom-dark',
-        codeLanguage:'javascript',
-        codeContent:`function add(a, b) {
+  name: "canvasStyle",
+  initialState: {
+    canvasPadding: "30px",
+    canvasBackGround: "#6B5ED9",
+    canvasWidth: "800px",
+    snippetFontFamily: `'JetBrains Mono', monospace`,
+    snippetMode: "dark",
+    snippetTheme: "prism-atom-dark",
+    codeLanguage: "javascript",
+    codeContent: `function add(a, b) {
             return a + b;
           }`,
-        canvasGradientBackgroundStart:'#6B5ED9',
-        canvasGradientBackgroundEnd:'#6B5ED9',
-        canvasGradientBackgroundAngle:'90',
-        snippetFileName:'Untitled - 1',
-        canvasColourType:'solid',
-        FontTheme:'prism-atom-dark',
+    canvasGradientBackgroundStart: "#6cbca4",
+    canvasGradientBackgroundEnd: "#1a4175",
+    canvasGradientBackgroundAngle: "100",
+    snippetFileName: "Untitled - 1",
+    canvasColourType: "linear",
+    FontTheme: "prism-atom-dark",
+  },
+  reducers: {
+    changecanvasPadding: (state, action) => {
+      state.canvasPadding = action.payload;
     },
-    reducers:{
-        changecanvasPadding:(state,action) =>{
-            state.canvasPadding = action.payload;
-        },
-        changecanvasBackGround:(state,action) =>{
-            state.canvasBackGround = action.payload;
-        },
-        changesnippetFontFamily:(state,action) =>{
-            state.snippetFontFamily = action.payload;
-        },
-        changesnippetMode:(state,action) =>{
-            state.snippetMode = action.payload;
-        },
-        changesnippetTheme:(state,action) =>{
-            state.snippetTheme = action.payload;
-        },
-        changecodeLanguage:(state,action) =>{
-            state.codeLanguage = action.payload;
-        },
-        changecodeContent:(state,action) =>{
-            state.codeContent = action.payload;
-        },
-        changecanvasWidth:(state,action) =>{
-            state.canvasWidth = action.payload;
-        },
-        changecanvasBackgroundStart:(state,action) =>{
-            state.canvasGradientBackgroundStart = action.payload;
-        },
-        changecanvasBackgroundEnd:(state,action) =>{
-            state.canvasGradientBackgroundEnd = action.payload;
-        },
-        changecanvasBackgroundAngle:(state,action) =>{
-            state.canvasGradientBackgroundAngle = action.payload;
-        },
-        changesnippetFileName:(state,action) =>{
-            state.snippetFileName = action.payload;
-        },
-        changecanvasColourType:(state,action) =>{
-            state.canvasColourType = action.payload;
-        },
-        changeFontTheme:(state,action) =>{
-            state.FontTheme = action.payload;
-        }
-    }
+    changecanvasBackGround: (state, action) => {
+      state.canvasBackGround = action.payload;
+    },
+    changesnippetFontFamily: (state, action) => {
+      state.snippetFontFamily = action.payload;
+    },
+    changesnippetMode: (state, action) => {
+      state.snippetMode = action.payload;
+    },
+    changesnippetTheme: (state, action) => {
+      state.snippetTheme = action.payload;
+    },
+    changecodeLanguage: (state, action) => {
+      state.codeLanguage = action.payload;
+    },
+    changecodeContent: (state, action) => {
+      state.codeContent = action.payload;
+    },
+    changecanvasWidth: (state, action) => {
+      state.canvasWidth = action.payload;
+    },
+    changecanvasBackgroundStart: (state, action) => {
+      state.canvasGradientBackgroundStart = action.payload;
+    },
+    changecanvasBackgroundEnd: (state, action) => {
+      state.canvasGradientBackgroundEnd = action.payload;
+    },
+    changecanvasBackgroundAngle: (state, action) => {
+      state.canvasGradientBackgroundAngle = action.payload;
+    },
+    changesnippetFileName: (state, action) => {
+      state.snippetFileName = action.payload;
+    },
+    changecanvasColourType: (state, action) => {
+      state.canvasColourType = action.payload;
+    },
+    changeFontTheme: (state, action) => {
+      state.FontTheme = action.payload;
+    },
+  },
 });
 
-export const {changeFontTheme,changecanvasColourType, changesnippetFileName, changecanvasBackgroundStart,changecanvasBackgroundEnd,changecanvasBackgroundAngle, changecanvasWidth, changecanvasPadding, changecanvasBackGround ,changesnippetFontFamily, changesnippetMode, changesnippetTheme, changecodeLanguage, changecodeContent} = CanvasConfigSlice.actions;
+export const {
+  changeFontTheme,
+  changecanvasColourType,
+  changesnippetFileName,
+  changecanvasBackgroundStart,
+  changecanvasBackgroundEnd,
+  changecanvasBackgroundAngle,
+  changecanvasWidth,
+  changecanvasPadding,
+  changecanvasBackGround,
+  changesnippetFontFamily,
+  changesnippetMode,
+  changesnippetTheme,
+  changecodeLanguage,
+  changecodeContent,
+} = CanvasConfigSlice.actions;
 
 export default CanvasConfigSlice.reducer;
